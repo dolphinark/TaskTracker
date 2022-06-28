42b9179d29efc620848c60ca806c5cafd74aae8a
Rita <ritahung19@gmail.com>
2022-06-28 14:29:43 +0800

    task list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Task from "./components/Task";
+import Button from "./components/Button";
 
 function App() {
+  const [data, setData] = useState([
+    {
+      id: 1,
+      text: "refund",
+      time: "Dec 12",
+      checked: true,
+    },
+    {
+      id: 2,
+      text: "buy ticket",
+      time: "Dec 12",
+      checked: false,
+    },
+    { id: 3, text: "movie", time: "June 11", checked: false },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header className="header">
+        <h1>Task Tracker</h1>
+        <Button />
       </header>
+      <Task
+        text={data.map((task) => (
+          <div className="task">
+            <h3>{task.text}</h3>
+            <p>{task.time}</p>
+          </div>
+        ))}
+      />
     </div>
   );
 }
---
+++ src/components/Task.js
@@ -0,0 +1,9 @@
+function Task(props) {
+  return (
+    <div className="div1">
+      <div className="div2">{props.text}</div>
+    </div>
+  );
+}
+
+export default Task;
